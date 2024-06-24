297c18d0ed186e6b16c44fa51e268b52bc2fa2cd
ahmed-Alsonaffi <ahmed.alsonaffi1@gmail.com>
2024-06-24 03:17:58 +0300

    improve login api

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,19 +5,21 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     
     public function login(Request $request)
     {
+        // return Hash::make($request['password']); 
+        
         $request->validate([
             'email' => 'required|email',
-            'password' => 'required|min:6'
+            'password' => 'required|min:8'
         ]);
-        $user = User::where("email","=",$request["email"])
-                ->where("password","=",$request["password"])->first();
-        if($user){
+        $user = User::where("email","=",$request["email"])->first();
+        if(Hash::check($request['password'], $user->password)){
             return response()->json([
                 'status'=>"success",
                 'user'=>$user
